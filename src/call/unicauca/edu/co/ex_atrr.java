75d7d2c9311ae3d40e58df33cd3c8fb2a21d918e
NICOLLEMON27 <142424875+NICOLLEMON27@users.noreply.github.com>
2023-08-30 10:57:17 -0500

    Update ex_atrr.java

--- src/call/unicauca/edu/co/ex_atrr.java
+++ src/call/unicauca/edu/co/ex_atrr.java
@@ -5,7 +5,7 @@ package call.unicauca.edu.co;
 public class ex_atrr {
     public String name="Nicolle montaño";
     private String document="1019023413";
-    public int year_hbd=16;
+    public int year_hbd=2006;
 
  
 }
